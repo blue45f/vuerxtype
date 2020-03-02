2cdc54580b96f819bae1dbd340b14cd0be660079
blue45f <1q2w3e4rZAQ!>
2020-03-02 12:06:19 +0900

    [AOD] 최소 실행 환경 추가

--- src/const/Services.ts
+++ src/const/Services.ts
@@ -2,9 +2,6 @@ import { HttpClient } from "../network/HttpClient";
 import { InjectService } from "../application-container/model/InjectService";
 import DependencyInjectId from "@/const/DependencyInjectId";
 import { LocaleJsonLoader } from "@/global-domain/loader";
-import { TransferApi } from "@/apis/transfer/TransferApi";
-import { TransferMapper } from "@/service/transfer/TransferMapper";
-import { TransferService } from "@/service/transfer/TransferService";
 
 export const SERVICES: Array<InjectService> = [
   {
@@ -16,17 +13,5 @@ export const SERVICES: Array<InjectService> = [
   {
     name: DependencyInjectId.LocaleJsonLoader,
     service: LocaleJsonLoader
-  },
-  {
-    name: DependencyInjectId.TransferApi,
-    service: TransferApi
-  },
-  {
-    name: DependencyInjectId.TransferMapper,
-    service: TransferMapper
-  },
-  {
-    name: DependencyInjectId.TransferService,
-    service: TransferService
   }
 ];
